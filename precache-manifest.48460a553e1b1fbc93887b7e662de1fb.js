2e07527d3cb452aa1fadd45d351bd0c4e06479b8
Senychak Nazar <nazarsenychak1@gmail.com>
2020-06-03 22:31:52 +0300

    Updates

---
+++ precache-manifest.48460a553e1b1fbc93887b7e662de1fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6c40bb6ee904d42c173",
+    "url": "/star-wars-app/static/css/main.e5616a88.chunk.css"
+  },
+  {
+    "revision": "b6c40bb6ee904d42c173",
+    "url": "/star-wars-app/static/js/main.b6c40bb6.chunk.js"
+  },
+  {
+    "revision": "b3b8a85abc6636448c5d",
+    "url": "/star-wars-app/static/js/1.b3b8a85a.chunk.js"
+  },
+  {
+    "revision": "47f42d09a35e53ea5631",
+    "url": "/star-wars-app/static/js/runtime~main.47f42d09.js"
+  },
+  {
+    "revision": "c6b0213c848c7b2c193efba6bbbd56a2",
+    "url": "/star-wars-app/index.html"
+  }
+];
